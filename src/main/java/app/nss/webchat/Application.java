5153f18f08bb32ba0b5d8327021d7575d3c459e6
Unknown <crystalsoldier488@gmail.com>
2023-08-11 23:45:59 +0200

    Enable Security

--- src/main/java/app/nss/webchat/Application.java
+++ src/main/java/app/nss/webchat/Application.java
@@ -2,11 +2,9 @@ package app.nss.webchat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class}) // Spring without security
-//@SpringBootApplication
+@SpringBootApplication
 @EnableCaching
 public class Application {
 
